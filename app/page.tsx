581cbc5121cd1ee6dacf784747c2ab7feabdcfdc
Manu Zeiher <manuzeiher@Mac-Studio-de-Manu.local>
2026-05-09 15:09:50 +0200

    Add landing page logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center px-6">
 
-      <h1 className="text-6xl font-bold mb-6">
-        MZVIBES
-      </h1>
-
-      <p className="text-zinc-400 text-center max-w-xl mb-10 text-lg">
-        AI tools for DJs, music lovers and creators.
-      </p>
+      <Image
+        src="/mz-logo.webp"
+        alt="MZVIBES"
+        width={220}
+        height={145}
+        priority
+      />
+<br/>
+      <p className="text-zinc-400 text-center max-w-xl mb-10 text-xl">
+  AI tools for DJs, music lovers and creators.
+</p>
 
       <a
         href="/app/playlistai"
-        className="bg-white text-black px-8 py-4 rounded-full font-semibold hover:scale-105 transition"
+       className="bg-white text-black px-10 py-4 rounded-full font-semibold hover:scale-105 transition"
       >
         Launch Playlist AI 🎵
       </a>
